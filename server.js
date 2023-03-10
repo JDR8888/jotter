bb9f5779d6ea2cf91cbd2add364f7f6c89867736
jake rudlong <jrudlong@gmail.com>
2023-03-09 22:00:10 -0600

    changes

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//rout for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 
@@ -16,8 +25,7 @@ app.use(express.static('public'));
 
 
 
-
-
+// listener 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
